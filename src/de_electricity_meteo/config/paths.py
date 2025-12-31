6ec28b80297a60ead6fde6418434381ac6043271
Hippolyte Debernardi <contact@hippolyte-debernardi.com>
2025-12-31 03:40:34 +0100

    removed some pydocstyle rules for now

--- src/de_electricity_meteo/config/paths.py
+++ src/de_electricity_meteo/config/paths.py
@@ -5,9 +5,7 @@ ROOT_DIR = Path(__file__).parent.parent.parent.parent
 DATA = ROOT_DIR / Path("data")
 DATA_BRONZE = DATA / Path("bronze")
 
-ODRE_REGISTRE_NATIONAL_INSTALLATIONS_BRONZE = (
-    DATA_BRONZE / "odre_registre_national_installations"
-)
+ODRE_REGISTRE_NATIONAL_INSTALLATIONS_BRONZE = DATA_BRONZE / "odre_registre_national_installations"
 
 CONFIG = ROOT_DIR / Path("src/de_electricity_meteo/config")
 LOGGER_CONFIG = CONFIG / "logger.yaml"
